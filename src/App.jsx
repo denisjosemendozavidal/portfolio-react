a3149ef1f62f14a487c7664a91021ba0e2e276d8
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-10-13 18:16:54 -0500

    Updating

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ function App() {
   const [istrueDe, setIstrueDe] = useState(false)
   const [istruePeace, setIstruePeace] = useState(false)
   const [istrueExp, setistrueExp] = useState(false)
+  const [mouseLeave, setMouseLeave] = useState(false)
+
+  const handleMouseLeave = () => {
+    setShowLetters(false)
+    setistrueFE(false)
+    setIstruePeace(false)
+  }
   
 
 
@@ -20,12 +27,12 @@ function App() {
   }
 
   const handleShowletters = () => {
-    setShowLetters(!showLetters)
+    setShowLetters(true)
     
   }
 
   const handleShowlettersFE = () => {
-    setistrueFE(!istrueFE)
+    setistrueFE(true)
   }
 
   const handleShowlettersDE = () => {
@@ -33,7 +40,7 @@ function App() {
   }
 
   const handleShowlettersPeace = () => {
-    setIstruePeace(!istruePeace)
+    setIstruePeace(true)
   }
 
   const handleistrueExp = () => {
@@ -50,10 +57,10 @@ function App() {
             <div className='pic'> {istrueExp ? <Experience/> : <img src="../images/imagenfondo.png" alt="" />}
             </div>
             <div className='introduction'>
-              <h2 className="hi" onMouseEnter={handleShowletters} onMouseLeave={handleShowletters} onClick={handleIsTrue}>{showLetters? "About me" : "Hi!"}</h2>
-              <h2 className="iam" onMouseEnter={handleShowlettersFE} onMouseLeave={handleShowlettersFE} onClick={handleistrueExp}>{istrueFE? "Experience" : "I'm"}</h2>
+              <h2 className="hi" onMouseEnter={handleShowletters} onMouseLeave={handleMouseLeave} onClick={handleIsTrue}>{showLetters? "About me" : "Hi!"}</h2>
+              <h2 className="iam" onMouseEnter={handleShowlettersFE} onMouseLeave={handleMouseLeave} onClick={handleistrueExp}>{istrueFE? "Experience" : "I'm"}</h2>
               <a href="../Files/DenisJoséMendozaVidalCV.pdf" target="_blank" download="Denis-Mendoza-CV" className='denis' onMouseEnter={handleShowlettersDE} onMouseLeave={handleShowlettersDE}>{istrueDe? "My CV" : "Denis"}</a> 
-              <h2 onMouseEnter={handleShowlettersPeace} onMouseLeave={handleShowlettersPeace} className='peace'>{istruePeace? 
+              <h2 onMouseEnter={handleShowlettersPeace} onMouseLeave={handleMouseLeave} className='peace'>{istruePeace? 
                       <span className='peace'>
                         <a href="https://github.com/denisjosemendozavidal" target="_blank"><img src="../images/github-logo.png" alt="" /></a>
                         <a href="https://www.linkedin.com/in/denis-mendoza-a085b518b" target="_blank"><img src="../images/linkedin-logo.png" alt="" /></a> 
