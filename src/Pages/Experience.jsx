2063bae0ea5a0aeecf64e5b50ac7415c5b16b8c7
denisjosemendozavidal <denismendozavidal@gmail.com>
2023-01-09 22:11:25 -0500

    Updating

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -6,6 +6,13 @@ const Experience = () => {
       <h2 className='experience-title'>Projects I've worked on</h2>
       <h4 className='experience-scroll-down-message'>Scroll down</h4>
       <div className='experience-galery'>
+        <div className='experience-galery-PERN-App'>
+          <h3 className='experience-galery-PERN-App-tittle'>PERN APP</h3>
+          <p>Used Tecnologies: PostgreSQL, Express, React.js, Node.js, among others</p>
+          <p>Idea behind it: Understanding how Front and Back relate to make a Full stack app possible</p>
+          <p>Please click on the image bellow to be redirected to its deployment. (link: "https://fe-pern-crud-users.denismendoza.site/")</p>
+          <a href="https://fe-pern-crud-users.denismendoza.site/" target="_blank"><img src="/images/PERNStack.png" alt="" /></a>
+        </div>
         <div className='experience-galery-RN-project'>
           <h3 className='experience-galery-RN-project-tittle'>React Native project</h3>
           <p>Used Tecnologies: React Native, expo go, among others</p>
@@ -26,14 +33,6 @@ const Experience = () => {
           <p>Idea behind it: The goal behind it was understanding more about how asynchronous requests and REST APIs work while customizing the rendering to user's interaction with the app.</p>
           <p>Please click on the image bellow to be redirected to its Netlify deployment. (link: "https://denis-mendoza-api-usage-with-randm.netlify.app/")</p>
           <a href="https://denis-mendoza-api-usage-with-randm.netlify.app/" target="_blank"><img src="/images/R-m-moving.gif" alt="" /></a>
-        </div>
-        <div className='experience-galery-Weather-App'>
-          <h3 className='experience-galery-Weather-App-tittle'>The Weather-App</h3>
-          <p>Used Tecnologies: React.js, REST APIs, Axios, among others</p>
-          <p>Idea behind it: Doing an API consumption using axios, app shows current weather conditions based on user's location</p>
-          <p>Disclaimer: The App will not move beyond the loading page if it is not allowed to pull user's current location.</p>
-          <p>Please click on the image bellow to be redirected to its Netlify deployment. (link: "https://denis-mendoza-apiusage.netlify.app/")</p>
-          <a href="https://denis-mendoza-apiusage.netlify.app/" target="_blank"><img src="/images/weather-app-preview.png" alt="" /></a>
         </div>
         <div className='experience-galery-Ecommerce'>
           <h3 className='experience-galery-Ecommerce-tittle'>Ecommerce Javascript Vanilla</h3>
