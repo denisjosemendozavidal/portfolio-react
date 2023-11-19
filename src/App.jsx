fee22a2ad581699630661b4f31303175ff274de6
denisjosemendozavidal <denismendozavidal@gmail.com>
2023-11-19 14:17:27 -0500

    Navbar, Hero and About me HMTL done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import Aboutme from "./Pages/Aboutme";
 import Experience from "./Pages/Experience";
 
+/*This site will have several components before getting to the App component*/
+
+//Navbar component starts here
 const Navbar = () => {
   return (
     <div className="navbar">
@@ -65,6 +68,9 @@ const Navbar = () => {
   );
 };
 
+// Navbar component ends here
+
+// Hero Section component Starts here
 const HeroSection = () => {
   return (
     <div className="hero-section">
@@ -74,9 +80,9 @@ const HeroSection = () => {
         </div>
         <div className="hero-section-text-subheader">
           <h2>
-            Hi! I'm Denis a Full Stack, (Front End React.js Heavy), Next.js web
-            developer with a natural ability to learn and solve problems. Let's
-            get in touch:
+            Hi! I'm Denis, your go-to React.js Front End Web Developer with a
+            special knack for learning and problem solving. Excited to team up?
+            Let’s chat:
           </h2>
         </div>
         <div className="hero-section-ctas-wrapper">
@@ -97,12 +103,12 @@ const HeroSection = () => {
             </div>
             <div className="hero-section-ctas-find-me-on-options-wrapper">
               <a href="">
-                <img src="../images/github-logo.png" alt="" />
-                GitHub
+                <img src="../images/whatsapp-logo.png" alt="" /> Whatsapp
               </a>
 
               <a href="">
-                <img src="../images/whatsapp-logo.png" alt="" /> Whatsapp
+                <img src="../images/github-logo.png" alt="" />
+                GitHub
               </a>
 
               <a href="">
@@ -123,11 +129,63 @@ const HeroSection = () => {
   );
 };
 
+// HeroSection component ends here
+
+// AboutMe component starts here
+const AboutMe = () => {
+  return (
+    <div className="about-me-section-wrapper">
+      <div className="about-me-section-title-description-wrapper">
+        <div className="about-me-section-title-wrapper">
+          <h2>About me</h2>
+        </div>
+        <div className="about-me-section-description-wrapper">
+          <p>
+            I’m a web enthusiast turned professional who found his calling in
+            the world of programming. Ever since I discovered the art of
+            creating, communicating, and developing web applications that
+            interact responsively with users, I’ve been completely hooked.
+            Programming for me isn’t just a job; it’s a passion that drives me
+            to continually learn and grow. My journey has taken me from being
+            captivated by the endless possibilities of web development to
+            leading projects that turn those possibilities into reality.
+          </p>
+        </div>
+      </div>
+      <div className="about-me-section-content-wrapper">
+        <div className="about-me-section-content-video-wrapper">
+          {" "}
+          <video src="/images/Video/Introduction.mp4" controls></video>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// AboutMe component ends here
+
+// Skills component Start here
+const Skills = () => {
+  return (
+    <div className="skills-wrapper">
+      <div className="skills-title-wrapper">
+        <h2>Skills and Knowledge</h2>
+      </div>
+      <div className="skills-cards-wrapper"></div>
+    </div>
+  );
+};
+
+// Skills component ends here
+
+//App component starts here
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeroSection />
+      <AboutMe />
+      <Skills />
     </div>
   );
 }
